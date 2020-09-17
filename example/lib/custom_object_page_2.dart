fb8c3d7bb93118f656a9849190c627c321fdcfe0
Anargu <anthony.arostegui@gmail.com>
2020-09-16 20:47:46 -0500

    demo updated

--- example/lib/custom_object_page_2.dart
+++ example/lib/custom_object_page_2.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:arkit_plugin/arkit_plugin.dart';
 import 'package:flutter/material.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
@@ -24,6 +26,7 @@ class _CustomObjectPage2State extends State<CustomObjectPage2> {
         body: Container(
           child: ARKitSceneView(
             showFeaturePoints: true,
+            showWorldOrigin: true,
             planeDetection: ARPlaneDetection.horizontal,
             onARKitViewCreated: onARKitViewCreated,
             enableTapRecognizer: true,
@@ -35,15 +38,47 @@ class _CustomObjectPage2State extends State<CustomObjectPage2> {
     this.arkitController = arkitController;
     // this.arkitController.onAddNodeForAnchor = _handleAddAnchor;
     node = ARKitReferencePostNode(
-        position: vector.Vector3(0, 0, -2),
-        scale: vector.Vector3(0.2, 0.2, 0.2),
+        position: vector.Vector3(0, 0.1, -4),
+        scale: vector.Vector3(1, 1, 1),
         description: 'hello',
         username: 'joe',
         raw: 'https://media.giphy.com/media/frNEzcyLLtuOwEtwzo/giphy.gif',
         type: 'IMAGE',
         likes: 4,
         views: 0);
+
+    final node2 = ARKitReferencePostNode(
+        position: vector.Vector3(2, 0, -2),
+        scale: vector.Vector3(1, 1, 1),
+        description: 'hello',
+        username: 'joe',
+        raw: 'Hello',
+        type: 'TEXT',
+        likes: 4,
+        views: 0);
+    final node3 = ARKitReferencePostNode(
+        position: vector.Vector3(1, 0, -2),
+        scale: vector.Vector3(1, 1, 1),
+        description: 'hello',
+        username: 'joe',
+        raw: 'https://media.giphy.com/media/xT5LMJmXRmrn3ft04w/giphy.gif',
+        type: 'IMAGE',
+        likes: 4,
+        views: 0);
+    final node4 = ARKitReferencePostNode(
+        position: vector.Vector3(3, 0, -2),
+        scale: vector.Vector3(1, 1, 1),
+        description: 'hello',
+        username: 'joe',
+        raw: 'GOOD BYE',
+        type: 'TEXT',
+        likes: 4,
+        views: 0);
     this.arkitController.addUIView(node);
+    this.arkitController.addUIView(node2);
+    this.arkitController.addUIView(node3);
+    this.arkitController.addUIView(node4);
+
     this.arkitController.onNodeTap = (nodes) {
       print('nodes ${nodes.toString()}');
     };
